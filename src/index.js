e9ec49310be0e2e385f1387e65a50ef2aa604f4f
James Batsell <jbfuturedev@gmail.com>
2020-12-22 21:38:25 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log('%c HI', 'color: firebrick')
+document.addeventListener("DOMCOntentLoaded", () => {
+  fetchDogs()
+  fetchBreeds()
+  let dropDown = document.querySelector('#breed-dropdown')
+    dropDown.addeventListener('change', (e) => {
+      let breedChildren = document.querySelector('#dog-breeds').children
+      let breedsArray = [...breedChildren]
+      console.log()
+    })
+}
